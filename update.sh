baa3db428f9bf70df2be8b951dc7257dfae3eafc
Ivan Carlos <ivan@ivancarlos.com.br>
2025-03-13 09:55:53 -0300

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while true; do
-  if [ -d .git ]; then
-    git pull
-  else
-    git clone --recurse-submodules -j8 https://github.com/ivancarlosti/parkingpage.git .
-  fi
-  sleep 1800  # Sleep for 30 minutes (1800 seconds)
-done
