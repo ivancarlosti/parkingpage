4e9c0e5551954f367db7c3d94d509ec782e2e862
Ivan Carlos <ivan@ivancarlos.com.br>
2025-03-17 00:08:59 -0300

    Update info.php

--- info.php
+++ info.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // Get the current URL
 $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 // Remove the 'pass' query parameter from the URL
